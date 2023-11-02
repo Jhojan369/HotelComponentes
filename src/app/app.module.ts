63f0040029837e176ed60357fe14c7fba43a90e7
Jhojan Stivens David <000541191@MED.UPB>
2023-11-02 16:54:58 -0500

    Hotel components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FechaComponent } from './componentes/fecha/fecha.component';
+import { HabitacionesComponent } from './componentes/habitaciones/habitaciones.component';
+import { DescripcionComponent } from './componentes/descripcion/descripcion.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { ContactosComponent } from './componentes/contactos/contactos.component';
+import { Toast } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FechaComponent,
+    HabitacionesComponent,
+    DescripcionComponent,
+    FooterComponent,
+    ContactosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    Toast
   ],
   providers: [],
   bootstrap: [AppComponent]
